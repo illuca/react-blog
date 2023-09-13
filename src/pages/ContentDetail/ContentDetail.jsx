2f9d65dea4f45e978964d350e6cd4871d666f828
illuca <xillucax@gmail.com>
2023-09-13 23:41:31 +1000

    DONE: egg install and router configuration

--- src/pages/ContentDetail/ContentDetail.jsx
+++ src/pages/ContentDetail/ContentDetail.jsx
@@ -5,13 +5,50 @@ import 'comm.css';
 import Header from 'components/Header/Header';
 import Author from 'components/Author/Author';
 import Footer from 'components/Footer/Footer';
+import ReactMarkdown from 'react-markdown';
+import MarkNav from 'markdown-navbar';
+import 'markdown-navbar/dist/navbar.css';
+
 
 const ContentDetail = () => {
+  let markdown = '# course ' +
+    '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+    '> Mditor is a simple markdown editor. \n\n' +
+    '**bold**\n\n' +
+    '*italic*`\n\n' +
+    '***bold italic***\n\n' +
+    '~~deleted~~ \n\n' +
+    '# p02:Hello World Vue3.0\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n' +
+    '***\n\n\n' +
+    '# p03:Vue3.0\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p04:Vue3.0\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '#5 p05:Vue3.0\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p06:Vue3.0\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '# p07:Vue3.0\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n' +
+    '``` var a=11; ```'
   return (
     <div>
       <Header/>
       <Row className="comm-main" type="flex" justify="center">
-        <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}  >
+        <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
           <div>
             <div className="bread-div">
               <Breadcrumb>
@@ -30,18 +67,27 @@ const ContentDetail = () => {
                 <span><CalendarOutlined/> 2023 Sep 7</span>
                 <span><FolderOutlined/>Article</span>
               </div>
-
-              <div className="detailed-content" >
-                The big companies make efforts to develop green energy not because they love the earth. But the electricity is expensive. For labor cost, you can fire them but for electricity, you have to mantain the equipment every month. The clients’ data is stored in those equipments.
+              <div className="detailed-content">
+                <ReactMarkdown
+                  children={markdown}
+                  escapeHtml={false}
+                />
               </div>
-
             </div>
 
           </div>
         </Col>
 
         <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
-          <Author />
+          <Author/>
+          <div className="detailed-nav comm-box">
+            <div className="nav-title">outline</div>
+            <MarkNav
+              className="article-menu"
+              source={markdown}
+              ordered={false}
+            />
+          </div>
         </Col>
       </Row>
       <Footer/>
